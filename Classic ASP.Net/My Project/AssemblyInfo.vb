24ab698ac1c7dcb2ec96fd677c8a431a7c0dc95a
Tejvir Dhami <Tejvir Dhami@DESKTOP-J1P39NR>
2021-09-08 08:38:34 -0400

    Add project files.

---
+++ Classic ASP.Net/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("Classic_ASP.Net")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Classic_ASP.Net")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0171bf46-2f7f-4cb6-807b-eecc91b68675")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Classic ASP.Net/index.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Language="javascript"%>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Classical ASP</title>
+    <script></script>
+    <style type="text/css">
+
+        
+        .auto-style2 {
+            
+            text-align:center;
+
+        }
+
+
+    </style>
+</head>
+<body>
+
+    <h1 class="auto-style2"><u>LIONS-CLUB</u></h1>
+
+    <form action =" " method="post" id="form1" runat="server">
+               
+        
+        <div style="background-color: aqua;
+                    text-align:center;
+                    padding:10px;
+                    border-radius: 10px;
+                    margin-left: 300px;
+                    margin-right: 300px;">
+
+            <h3>Username : <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox></h3>
+            <h3>Password : <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox></h3>
+            <asp:Button ID="Enter" runat="server" Text="Enter" /><asp:Button ID="Clear" runat="server" Text="Clear" />
+
+
+        </div>
+
+    </form>
+</body>
+</html>
